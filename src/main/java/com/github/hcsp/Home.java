66b5e63e9a46a4682d28a7dd3ccc641478ef454f
catsme <845058547@qq.com>
2019-06-09 10:49:39 +0800

    分别对cat1,cat2取对应的名字。

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -17,7 +17,7 @@ public class Home {
     }
 
     public static void createTwoCats() {
-        cat1 = new Cat("Tom");
-        cat2 = cat1;
+        cat1 = new Cat("Jerry");
+        cat2 = new Cat("Tom");
     }
 }
