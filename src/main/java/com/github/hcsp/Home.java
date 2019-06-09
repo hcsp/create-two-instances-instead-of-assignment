f9fecb44d01655def1d5a8d3f7a1df38ce2b6cb3
Bo Zhang <bo@gradle.com>
2019-06-09 11:01:00 +0800

    Remove ambiguous description

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -8,16 +8,12 @@ public class Home {
 
     public static void main(String[] args) {
         createTwoCats();
-
-        System.out.println("1: " + cat1 + ", 2:" + cat2);
-
-        cat1.name = "Jerry";
-
         // We want to create two cats, not one cat with two references
-        // Fix createTwoCats() method to make the problem output "1: Cat(Jerry), 2: Cat(Tom)"
+        // Fix createTwoCats() method to make the problem output "1: Cat(Tom), 2: Cat(Tom),
+        // cat1==cat2: false"
         // 我们希望创建两只猫，而不是一只猫的两个引用
-        // 请修改createTwoCats()方法，使得程序输出"1: Cat(Jerry), 2: Cat(Tom)"
-        System.out.println("1: " + cat1 + ", 2:" + cat2);
+        // 请修改createTwoCats()方法，使得程序输出"1: Cat(Tom), 2: Cat(Tom), cat1==cat2: false"
+        System.out.println("1: " + cat1 + ", 2:" + cat2 + ", cat1==cat2: " + (cat1 == cat2));
     }
 
     public static void createTwoCats() {
