29a6afa631228783e69a7f7269673f10922528ed
Bo Zhang <bo@gradle.com>
2019-06-15 18:43:07 +0800

    Update repo

--- src/test/java/com/github/hcsp/HomeTest.java
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -1,18 +1,16 @@
 package com.github.hcsp;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class HomeTest {
     @Test
     public void referencePassingTest() {
-        assertEquals(null, Home.cat1);
-        assertEquals(null, Home.cat2);
+        Assertions.assertEquals(null, Home.cat1);
+        Assertions.assertEquals(null, Home.cat2);
         Home.createTwoCats();
-        assertEquals("Cat(Tom)", Home.cat1.toString());
-        assertEquals("Cat(Tom)", Home.cat2.toString());
-        assertTrue(Home.cat1 != Home.cat2);
+        Assertions.assertEquals("Cat(Tom)", Home.cat1.toString());
+        Assertions.assertEquals("Cat(Tom)", Home.cat2.toString());
+        Assertions.assertTrue(Home.cat1 != Home.cat2);
     }
 }
