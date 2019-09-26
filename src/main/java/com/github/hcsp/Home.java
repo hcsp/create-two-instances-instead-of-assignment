afb555730c33e3084e22736ffd0f6da2b982da25
xuepan-summer <54898607+xuepan-summer@users.noreply.github.com>
2019-09-26 17:14:14 +0800

    两只猫分别的引用 (#84)
    两只猫分别的引用 (#84)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -18,6 +18,11 @@ public class Home {
 
     public static void createTwoCats() {
         cat1 = new Cat("Tom");
-        cat2 = cat1;
+//        System.out.println(cat1);
+//        cat2 = cat1;
+//        System.out.println(cat1 == cat2);
+        cat2 = new Cat("Tom");
+//        System.out.println(cat2);
+//        System.out.println(cat1 == cat2);
     }
 }
