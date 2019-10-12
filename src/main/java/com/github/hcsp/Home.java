e8a6479aef6bf2f220be2092897656debf0ae6ec
dqprecisian <1352034864@qq.com>
2019-10-12 17:53:39 +0800

    创建两个名字相同的对象，但却不能相等。

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -13,6 +13,8 @@ public class Home {
         // cat1==cat2: false"
         // 我们希望创建两只猫，而不是一只猫的两个引用
         // 请修改createTwoCats()方法，使得程序输出"1: Cat(Tom), 2: Cat(Tom), cat1==cat2: false"
+        cat1 = new Cat("Tom");
+        cat2 = new Cat("Tom");
         System.out.println("1: " + cat1 + ", 2:" + cat2 + ", cat1==cat2: " + (cat1 == cat2));
     }
 
