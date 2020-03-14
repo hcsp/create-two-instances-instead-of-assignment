c4ae9fef0b29701882ce6d5caf8a3e8b7fa963c9
Tonny Ge <2454424391@qq.com>
2020-03-14 14:44:07 +0800

    2cats (#176)
    2cats (#176)

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -9,7 +9,8 @@ public class Home {
     public static void main(String[] args) {
         createTwoCats();
         // We want to create two cats, not one cat with two references
-        // Fix createTwoCats() method to make the problem output "1: Cat(Tom), 2: Cat(Tom),
+        // Fix createTwoCats() method to make the problem output "1: Cat(Tom), 2:
+        // Cat(Tom),
         // cat1==cat2: false"
         // 我们希望创建两只猫，而不是一只猫的两个引用
         // 请修改createTwoCats()方法，使得程序输出"1: Cat(Tom), 2: Cat(Tom), cat1==cat2: false"
@@ -18,6 +19,6 @@ public class Home {
 
     public static void createTwoCats() {
         cat1 = new Cat("Tom");
-        cat2 = cat1;
+        cat2 = new Cat("Tom");
     }
 }
